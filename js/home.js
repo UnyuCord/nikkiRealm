0c5d1a47e51437f6d6b0a8b9fc8195b05b25dd8e
realNikki <nikecoes@gmail.com>
2025-11-28 03:50:03 +0100

    arbitrary cloud generation that sometimes sucks

---
+++ js/home.js
@@ -0,0 +1,36 @@
+let currentClouds = 0;
+const numberOfMaximumClouds = 5;
+
+function createCloud() {
+
+    if (numberOfMaximumClouds <= currentClouds) return;
+
+    const cloudContainer = document.getElementById("top-cover");
+    const imageNode = new Image()
+    imageNode.src = './resources/cloud.png';
+    const cloudAnimationDurationMinimum = 8;
+    const cloudAnimationDurationMaximum = 12;
+    const cloudAnimationDuration = getRandomNumberInRange(cloudAnimationDurationMinimum, cloudAnimationDurationMaximum);
+
+    imageNode.style.pointerEvents = "none";
+    imageNode.style.userSelect = "none";
+    imageNode.style.position = 'absolute';
+    imageNode.style.animation= 'moveRightToLeft'
+    imageNode.style.animationTimingFunction = "linear"
+
+    imageNode.onanimationend = function () {
+        if (currentClouds > 0) currentClouds--;
+        imageNode.remove();
+    }
+    imageNode.style.left = '100%';
+    imageNode.style.top =
+        `${getRandomNumberInRange(imageNode.height, (cloudContainer.getBoundingClientRect().height - imageNode.height))}px`
+    imageNode.style.animationDuration = cloudAnimationDuration + 's';
+    currentClouds++;
+
+    cloudContainer.appendChild(imageNode);
+}
+
+function getRandomNumberInRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
